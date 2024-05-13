7e6d60d126fe3d1c0817a2f2123a726e8a62f5cd
victorsootho <soothotsoenyane@gmail.com>
2024-05-13 16:29:06 +0200

    added CRAWL tasks and minutes active

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use chrono::{Datelike, Duration, NaiveDateTime, Utc};
+use std::io;
 
 fn main() {
     let current_time = Utc::now() + Duration::hours(2);
@@ -25,5 +26,41 @@ fn main() {
     println!(
         "You have {} hours and {} minutes left.",
         hours_left, minutes_left
-    )
+    );
+
+    println!("Here Are Today's and Tomorrow's Deadlines");
+
+    println!("Add Today's Tasks");
+
+    let mut tasks = vec![
+        ("Coding", 0),
+        ("Reading", 0),
+        ("Audio", 0),
+        ("Writing", 0),
+        ("Learning", 0),
+    ];
+    let mut total_productivity_minutes: u64 = 0;
+
+    println!("Enter durations for Today's Tasks");
+    for (_i, (task, duration)) in tasks.iter_mut().enumerate() {
+        println!("Enter duration for {} (in minutes): ", task);
+        let mut crawl_input = String::new();
+        io::stdin()
+            .read_line(&mut crawl_input)
+            .expect("Failed to read line");
+        let input: u64 = crawl_input.trim().parse().expect("Invalid duration");
+
+        *duration = input;
+        total_productivity_minutes += input;
+    }
+
+    let hours_productive = total_productivity_minutes / 60;
+    let minutes_productive = total_productivity_minutes % 60;
+    println!(
+        "You have been productive for {} hours and {} minutes",
+        hours_productive, minutes_productive
+    );
+    for (i, (task, duration)) in tasks.iter().enumerate() {
+        println!("Task {}: {} - {} minutes", i + 1, task, duration);
+    }
 }
